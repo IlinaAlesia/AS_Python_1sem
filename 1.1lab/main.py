32c345d02240dea13c3ca4e0b2544aec896866c0
IlinaAlesia <ilinaalesia@gmail.com>
2025-11-02 19:21:25 +0300

    Update main.py

--- 1.1lab/main.py
+++ 1.1lab/main.py
@@ -1,13 +1,6 @@
-# Задание 12 из ЕГЭ-2025 профиль [https://math-ege.sdamgia.ru/test?id=77409203&ysclid=mgqhp19pfb346776915]
-# Найдите наименьшее значение функции y=9x-9ln(x+11)+7 на отрезке [-10,5;0] 
-import numpy as np
-import math
-def f(x):
-    "Исходная функция y=9x-9ln(x+11)+7"
-    return 9*x-9*math.log(x+11)+7
-def f_derivative(x):
-    "Производная функции"
-    return 3*x*2-6*x
-def f_second_derivate(x):
-    "Вторая производная функции"
-    return 6*x-6
+# Задание 5 из ЕГЭ-2025 профиль [https://ege.sdamgia.ru/problem?id=665292]
+# Помещение освещается фонарём с тремя лампами. Вероятность перегорания одной лампы в течение года равна 0,2. Найдите вероятность того, что в течение года хотя бы одна лампа не перегорит.
+peregorit = 0.2
+wseperegoryat = peregorit**3
+result = 1-wseperegoryat
+print(result)
