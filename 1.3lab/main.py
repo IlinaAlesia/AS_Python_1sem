613b7d350b3337773668e51f561c71918b4691a3
IlinaAlesia <ilinaalesia@gmail.com>
2025-11-02 20:12:30 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+# 4 вариант Ильина
+import math
+n = int(input("Натуральное число n: "))
+sum = 0
+print("Вычисление суммы:")
+for x in range(1, n+1):
+    y= 1/math.sin(x)
+    sum += y
+    print ("1/sin(" + str(x) + ")= " + str(round(y,6)))
+print("\nИтоговая сумма: {}".format(round(sum,6)))
+
