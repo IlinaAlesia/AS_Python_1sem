a5733664c7daf17abca55948a7941d4ce71396a7
IlinaAlesia <ilinaalesia@gmail.com>
2025-11-02 20:25:04 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+# 4 задание Ильина
+print("Исходный список:", numbers)
+even=[]
+for i in range(len(numbers)):
+    if numbers[i]%2==0:
+      even.append(numbers[i])
+odd=[]
+for i in range(len(numbers)-1,-1,-1):
+    if numbers[i]%2==1:
+      odd.append(numbers[i])
+print("\nЧётные:", even)
+print("Нечётные:",odd)
