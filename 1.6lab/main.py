52773ff8f2c31ec33efb4b06817e091341735808
IlinaAlesia <ilinaalesia@gmail.com>
2025-11-02 22:08:19 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+# 4 вариант Ильина
+str1 = "первая"
+str2 = "вторая"
+new_str1 = str2[0] + str2[1] + str1[2] + str1[3] + str1[4] + str1[5] 
+new_str2 = str1[0] + str1[1] + str2[2] + str2[3] + str2[4] + str2[5]
+result = new_str1 + " " + new_str2
+print(result)
