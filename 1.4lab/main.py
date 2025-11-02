d25f2358e03157045610e3e4a71659bcd27474e6
IlinaAlesia <ilinaalesia@gmail.com>
2025-11-02 20:30:46 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -4,9 +4,11 @@ even=[]
 for i in range(len(numbers)):
     if numbers[i]%2==0:
       even.append(numbers[i])
+        even.sort()
 odd=[]
 for i in range(len(numbers)-1,-1,-1):
     if numbers[i]%2==1:
       odd.append(numbers[i])
+        odd.sort(reverse=True)
 print("\nЧётные:", even)
 print("Нечётные:",odd)
