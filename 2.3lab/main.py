eddbd85ebcdb002f61bf0f0b78741f6daeedb9b8
IlinaAlesia <ilinaalesia@gmail.com>
2025-11-18 12:26:18 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    pass
+# 4 вариант Ильина
+def Transp(A):
+    M = len(A)
+    return [[A[j][i] for j in range(M)] for i in range(M)]
+# Пример использования
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+result = Transp(matrix)
+for row in result:
+    print(row)
