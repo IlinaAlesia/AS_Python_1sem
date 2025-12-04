7cbaa8feae9a985f59b33855d6af8630bcd1397e
IlinaAlesia <ilinaalesia@gmail.com>
2025-12-04 20:23:25 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,22 @@
-if __name__ == "__main__":
-    pass
+def find_sum_of_min_max(filename='f'):
+    try:
+        with open(filename, 'r') as file:
+            numbers = []
+            for line in file:
+                for token in line.split():
+                    try:
+                        num = float(token)
+                        numbers.append(num)
+                    except ValueError:
+                        continue
+            
+            if not numbers:
+                return None
+            
+            min_val = min(numbers)
+            max_val = max(numbers)
+            return min_val + max_val
+            
+    except FileNotFoundError:
+        print(f"Файл '{filename}' не найден.")
+        return None
