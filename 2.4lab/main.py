e0600de0308ba409dc2757803545f07633d57643
IlinaAlesia <ilinaalesia@gmail.com>
2025-12-04 20:02:52 +0300

    Update main.py

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -21,8 +21,31 @@ def transform_list(numbers, transform_function=None):
     # Если функция преобразования указана, применяем её к каждому элементу  
     if transform_function is not None:  
         result = [transform_function(x) for x in result]  
+    
     return result  
 # в)
 def sum_positive_numbers(*args):  
     return sum(num for num in args if num > 0)
 # г)
+def common_keys(**kwargs):  
+    # Если нет словарей, возвращаем пустой словарь  
+    if not kwargs:  
+        return {}  
+      
+    # Получаем список всех словарей  
+    dicts = kwargs.values()  
+      
+    # Находим общие ключи во всех словарях  
+    common = set(dicts.keys())  
+    for d in dicts[1:]:  
+        common.intersection_update(d.keys())  
+      
+    # Создаем результирующий словарь с общими ключами и значениями  
+    result = {}  
+    for key in common:  
+        # Проверяем, что значения одинаковы во всех словарях  
+        value = dicts[key]  
+        if all(d[key] == value for d in dicts[1:]):  
+            result[key] = value  
+      
+    return result
