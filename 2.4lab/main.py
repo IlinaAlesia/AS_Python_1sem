14c3d34a53d2007dba00ebdf54b3cfb63c298f8f
IlinaAlesia <ilinaalesia@gmail.com>
2025-12-04 19:58:39 +0300

    Update main.py

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+# 4 вариант Ильина
+# а)
+def calculate(a, b, operation='add'):  
+    if operation == 'add':  
+        return a + b  
+    elif operation == 'subtract':  
+        return a - b  
+    elif operation == 'multiply':  
+        return a * b  
+    elif operation == 'divide':  
+        if b == 0:  
+            raise ValueError("Деление на ноль невозможно")  
+        return a / b  
+    else:  
+        raise ValueError("Неизвестная операция. Допустимые значения: 'add', 'subtract', 'multiply', 'divide'")
+# б)
